8319eb5a15d0e8e1b8d3a881a174b81647c05747
Alhoussen <traorealhoussen@gmail.com>
2024-10-12 13:29:38 +0000

    say my name fonction

--- my_flet_app/main.py
+++ my_flet_app/main.py
@@ -3,14 +3,17 @@ import time
 
 
 def main(page: ft.Page):
-    def add_clicked(e):
-        page.add(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        new_task.focus()
-        new_task.update()
+    def btn_click(e):
+        if not txt_name.value:
+            txt_name.error_txt = "Please enter your name"
+            page.update
+        else:
+            name = txt_name.value
+            page.clean()
+            page.add(ft.Text(f"Hello, {name}!"))
 
-    new_task = ft.TextField(hint_text="What's needs to be done?", width=300)
-    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
+    txt_name = ft.TextField(label="Your name")
+    page.add(txt_name, ft.ElevatedButton("Say hello!", on_click=btn_click))
 
 
 
